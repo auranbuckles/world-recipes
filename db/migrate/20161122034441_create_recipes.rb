711dfba827896edc4ffcf932c405c50b2438097a
auranbuckles <auran59@gmail.com>
2016-11-21 23:04:57 -0500

    created database tables

---
+++ db/migrate/20161122034441_create_recipes.rb
@@ -0,0 +1,16 @@
+class CreateRecipes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.integer :difficulty
+      t.integer :time
+      t.integer :servings
+      t.string :ingredients, array: true
+      t.string :directions, array: true
+      t.integer :category_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
