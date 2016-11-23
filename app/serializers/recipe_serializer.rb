87bc156c91f980066fba25d3f3346ebfe3d3c630
auranbuckles <auran59@gmail.com>
2016-11-23 10:48:14 -0500

    added description to recipe serializer and category view page

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,5 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :difficulty, :time, :servings, :ingredients, :directions
+  attributes :id, :title, :difficulty, :time, :servings, :description, :ingredients, :directions
 
   belongs_to :category
   belongs_to :author, class_name: 'User'
