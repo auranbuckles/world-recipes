f7754af0c9f4ea6db83829021b69dad61fd76607
auranbuckles <auran59@gmail.com>
2016-11-25 14:49:08 -0500

    added user log in fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
+	has_many :recipes, foreign_key: 'author_id'
+  has_many :favorites
+  has_many :favorite_recipes, through: :favorites, source: :recipe
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :recipes, foreign_key: 'author_id'
-  has_many :favorites
-  has_many :favorite_recipes, through: :favorites, source: :recipe
 end
