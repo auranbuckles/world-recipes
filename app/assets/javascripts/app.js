fdde27781d67cc1d44c108870c31cbab3b2c6688
auranbuckles <auran59@gmail.com>
2016-11-25 14:47:30 -0500

    added user authentication

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,12 +1,15 @@
 (function() {
 	'use strict';
 
-	function RecipeController ($scope) {
-		$scope.name = "Auran"
-	}
-
 	angular
 		.module('app', ['ui.router', 'templates', 'Devise'])
-		.controller('RecipeController', RecipeController)
+		.config(function(AuthProvider) {
+      // Configure Auth service with AuthProvider
+    })
+    .controller('AuthController', function(Auth) {
+      // Use your configured Auth service.
+      console.log(Auth._currentUser);
+      console.log(Auth.isAuthenticated());
+    });
 
 }());
