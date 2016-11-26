f7f4c17624d4032475eaa8791bc28f93fab1863d
auranbuckles <auran59@gmail.com>
2016-11-26 16:10:01 -0500

    added http get to categories index

--- app/assets/javascripts/categories/CategoryService.js
+++ app/assets/javascripts/categories/CategoryService.js
@@ -1,5 +1,9 @@
 function CategoryService($http) {
 
+	this.getCategories = function() {
+		return $http.get('http://localhost:3000/categories')
+	};
+
 	this.getCategory = function(id) {
 		return $http.get('http://localhost:3000/categories/' + id)
 	};
