027f2d831f8c3dcf5fe737e56918cae46edbd3d5
auranbuckles <auran59@gmail.com>
2016-11-26 23:13:37 -0500

    cleaned up comments and unused code

--- app/assets/javascripts/recipes/NewRecipeController.js
+++ app/assets/javascripts/recipes/NewRecipeController.js
@@ -16,27 +16,30 @@
 	    	time: this.time,
 	    	servings: this.servings,
 	    	description: this.description,
-	    	ingredients: [this.ingredients[0]],
-	    	directions: '["direction-1", "direction-2"]',
+	    	ingredients: this.ingredients,
+	    	directions: this.directions,
 	    	category_id: this.category.id
 	    };
 
-	    data.ingredients.push("ingredient1", "ingredient2")
+	    // data.directions = JSON.stringify(data.directions);
+
+	    // addIngredient("ingredient1", "ingredient2");
+	    debugger;
 
 	    RecipeService.createRecipe(data);
 	    $location.path('profile');
 	  };
-  
-  	vm.addIngredient = function() {
-  		debugger;
-	    var newIngredientNo = data.ingredients.length + 1;
-	    data.ingredients.push('');
-	  };
+
+  	// vm.addIngredient = function() {
+  	// 	debugger;
+	  //   var newIngredientNo = data.ingredients.length + 1;
+	  //   data.ingredients.push('');
+	  // };
 	    
-	  vm.removeIngredient = function() {
-	    var lastIngredient = vm.data.ingredients.length - 1;
-	    data.ingredients.splice(lastIngredient);
-	  };
+	  // vm.removeIngredient = function() {
+	  //   var lastIngredient = vm.data.ingredients.length - 1;
+	  //   data.ingredients.splice(lastIngredient);
+	  // };
 	}
 
 	angular
