a8ac89ad77e7d6d2c08fde3a2739008d56e31b5a
auranbuckles <auran59@gmail.com>
2016-11-26 23:12:52 -0500

    added filter

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -10,6 +10,20 @@
       // Use your configured Auth service.
       console.log(Auth._currentUser);
       console.log(Auth.isAuthenticated());
-    });
+    })
+    .filter('titleize', function() {
+	    return function(input, all) {
+	      var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
+	      return (!!input) ? input.replace(reg, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+	    }
+			// return function(input) {
+			// 	var words = input.split(' ');
+			// 	for (var i = 0; i < words.length; i++) {
+			// 		words[i] = words[i][0].toUpperCase() +
+			// 		words[i].substr(1);
+			// 	};
+			// 	return words.join(' ');
+			// }
+	  });
 
 }());
