bcb0fe42383ec7f9e8c9f6a91765a7b9f8ca5063
auranbuckles <auran59@gmail.com>
2016-11-30 14:06:46 -0500

    built basic variable and functions for ingredient field

--- app/assets/javascripts/controllers/NewRecipeController.js
+++ app/assets/javascripts/controllers/NewRecipeController.js
@@ -4,19 +4,24 @@
 	function NewRecipeController ($state, RecipeService, CategoryService) {
 		var vm = this;
 
-		// CategoryService.getCategories()
-		// 	.then(function(response) {
-		// 		vm.categories = response.data;
-		// 	});
+		CategoryService.getCategories()
+			.then(function(response) {
+				vm.categories = response.data;
+			});
 
-		this.ingredients = ["ingredient 1"];
-		// this.input counter = 0;
-		// this.$watch('telephone', function (value) {
-  //     console.log(value);
-  //   }, true);
+	  vm.choices = [{id: 'choice1'}, {id: 'choice2'}];
+	  
+	  vm.addNewChoice = function() {
+	    var newItemNo = vm.choices.length+1;
+	    vm.choices.push({'id':'choice'+newItemNo});
+	  };
+	    
+	  vm.removeChoice = function() {
+	    var lastItem = vm.choices.length-1;
+	    vm.choices.splice(lastItem);
+	  };
 
 	  vm.addRecipe = function() {
-	  	// this.ingredients = ["ingredient 1"];
 
 	    var data = {
 	    	title: this.title,
@@ -24,30 +29,27 @@
 	    	time: this.time,
 	    	servings: this.servings,
 	    	description: this.description,
-	    	ingredients: this.ingredients.join("\r\n"),
+	    	ingredients: this.choices,
 	    	directions: ["direction1", "direction2"].join("\r\n"),
 	    	category_id: this.category.id
 	    };
 
 	    // data.directions = JSON.stringify(data.directions);
-
-	    // addIngredient("ingredient1", "ingredient2");
 	    debugger;
 
 	    RecipeService.createRecipe(data);
 	    $state.go('home.profile');
 	  };
 
-  	vm.addIngredient = function() {
-  		debugger;
-	    var newIngredientNo = data.ingredients.length + 1;
-	    data.ingredients.push('');
-	  };
+  	// vm.addIngredient = function() {
+	  //   var newIngredientNo = data.ingredients.length + 1;
+	  //   data.ingredients.push('');
+	  // };
 	    
-	  vm.removeIngredient = function() {
-	    var lastIngredient = vm.data.ingredients.length - 1;
-	    data.ingredients.splice(lastIngredient);
-	  };
+	  // vm.removeIngredient = function() {
+	  //   var lastIngredient = vm.data.ingredients.length - 1;
+	  //   data.ingredients.splice(lastIngredient);
+	  // };
 	}
 
 	angular
