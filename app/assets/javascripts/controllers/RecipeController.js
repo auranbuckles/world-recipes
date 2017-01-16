63202f403a06fea8bd6565b2fc56ada1d2eb63de
auranbuckles <auran59@gmail.com>
2017-01-15 23:15:48 -0500

    upvote button on home page is able to add upvotes to recipe in database

--- app/assets/javascripts/controllers/RecipeController.js
+++ app/assets/javascripts/controllers/RecipeController.js
@@ -1,13 +1,34 @@
 (function() {
 	'use strict';
 
-	function RecipeController (RecipeService) {
+	function RecipeController (RecipeService, $state) {
 		var vm = this;
+		
+		vm.reloadRoute = function() {
+	    $state.reload();
+	    console.log("reloading")
+		};
 
 		RecipeService.getRecipes()
 			.then(function(response) {
 				vm.recipes = response.data;
+
+				// vm.recipes.forEach (function (recipe) {
+				// 	recipe.id = 0;
+				// });
 			});
+
+		vm.upvote = function(recipe) {
+			var data = {
+				upvotes: recipe.upvotes + 1
+			}
+			RecipeService.updateRecipe(recipe.id, data);
+			console.log("added 1 upvote");
+			vm.reloadRoute();
+
+			// recipe.counter += 1;
+			// console.log("added 1 upvote")
+		}
 
 	}
 
