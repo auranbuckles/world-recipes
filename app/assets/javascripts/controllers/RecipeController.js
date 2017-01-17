fcbce55952d7edb4e70ca845bb9a03382dd4af95
auranbuckles <auran59@gmail.com>
2017-01-17 12:57:55 -0500

    tidied up code

--- app/assets/javascripts/controllers/RecipeController.js
+++ app/assets/javascripts/controllers/RecipeController.js
@@ -3,6 +3,7 @@
 
 	function RecipeController (RecipeService, $state) {
 		var vm = this;
+		vm.loading = true;
 		
 		vm.reloadRoute = function() {
 	    $state.reload();
@@ -11,27 +12,18 @@
 
 		RecipeService.getRecipes()
 			.then(function(response) {
+				vm.loading = false;
 				vm.recipes = response.data;
-
-				// vm.recipes.forEach (function (recipe) {
-				// 	recipe.id = 0;
-				// });
 			});
 
 		vm.upvote = function(recipe) {
 			var data = {
 				upvotes: recipe.upvotes + 1
 			}
-			var i = vm.recipes.indexOf(recipe)
-			vm.recipes.splice(i, 1)
 			RecipeService.updateRecipe(recipe.id, data)
 				.then(function(data) {
-					vm.recipes.push(data)
 				});
-			// vm.reloadRoute();
-
-			// recipe.counter += 1;
-			// console.log("added 1 upvote")
+			recipe.upvotes++;
 		}
 
 	}
